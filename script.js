1ceafa9eb4bd3abaf343a2ecf369268760be6a74
NSeraj11 <99372161+NSeraj11@users.noreply.github.com>
2022-07-05 08:53:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -27,42 +27,42 @@ function makeSound(key) {
 
   switch (key) {
     case "lion":
-      var lion = new Audio("sounds/lion.mp3");
+      var lion = new Audio("lion.mp3");
       lion.play();
       break;
 
     case "eagle":
-      var eagle = new Audio("sounds/eagle.mp3");
+      var eagle = new Audio("eagle.mp3");
       eagle.play();
       break;
 
     case "wolf":
-      var wolf = new Audio("sounds/wolf.mp3");
+      var wolf = new Audio("wolf.mp3");
       wolf.play();
       break;
 
     case "chicken":
-      var chicken = new Audio("sounds/chicken.mp3");
+      var chicken = new Audio("chicken.mp3");
       chicken.play();
       break;
 
     case "cheetah":
-      var cheetah = new Audio("sounds/cheetah2.mp3");
+      var cheetah = new Audio("cheetah2.mp3");
       cheetah.play();
       break;
 
     case "mockingbird":
-      var mockingbird = new Audio("sounds/mockingbird.mp3");
+      var mockingbird = new Audio("mockingbird.mp3");
       mockingbird.play();
       break;
 
     case "giraffe":
-      var giraffe = new Audio("sounds/giraffe.mp3");
+      var giraffe = new Audio("giraffe.mp3");
       giraffe.play();
       break;
 
     case "elephant":
-      var elephant = new Audio("sounds/elephant.mp3");
+      var elephant = new Audio("elephant.mp3");
       elephant.play();
       break;
 
