d2ca7b85a044f8c11f3232055152c78841c14014
NSeraj11 <99372161+NSeraj11@users.noreply.github.com>
2022-07-05 08:50:12 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+var numberofAnimals = document.querySelectorAll(".animal").length;
+
+for (var i = 0; i < numberofAnimals; i++) {
+
+  document.querySelectorAll(".animal")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML.toLowerCase();
+
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+
+  });
+}
+
+
+
+document.addEventListener("keypress",function(event){
+
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+
+});
+
+function makeSound(key) {
+
+  switch (key) {
+    case "lion":
+      var lion = new Audio("sounds/lion.mp3");
+      lion.play();
+      break;
+
+    case "eagle":
+      var eagle = new Audio("sounds/eagle.mp3");
+      eagle.play();
+      break;
+
+    case "wolf":
+      var wolf = new Audio("sounds/wolf.mp3");
+      wolf.play();
+      break;
+
+    case "chicken":
+      var chicken = new Audio("sounds/chicken.mp3");
+      chicken.play();
+      break;
+
+    case "cheetah":
+      var cheetah = new Audio("sounds/cheetah2.mp3");
+      cheetah.play();
+      break;
+
+    case "mockingbird":
+      var mockingbird = new Audio("sounds/mockingbird.mp3");
+      mockingbird.play();
+      break;
+
+    case "giraffe":
+      var giraffe = new Audio("sounds/giraffe.mp3");
+      giraffe.play();
+      break;
+
+    case "elephant":
+      var elephant = new Audio("sounds/elephant.mp3");
+      elephant.play();
+      break;
+
+    default: console.log(key);
+  }
+}
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+}
